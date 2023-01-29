ee1b180f4c8940d3b9d19fe67ee401ecf7c10ed3
Gautam Gupta <gautam@askmetechnologies.com>
2023-01-29 15:47:56 +0530

    Empty Branch

---
+++ gradedAssignment1/SuperDepartment.java
@@ -0,0 +1,36 @@
+package gradedAssignment1;
+
+public class SuperDepartment {
+	
+	/**
+	 * Method Name : departmentName
+	 * @return fixed string "Super Department".
+	 */
+	public String departmentName() {
+		return "Super Department";
+	}
+	
+	/**
+	 * Method Name : getTodaysWork
+	 * @return fixed string "No work as of now".
+	 */
+	public String getTodaysWork() {
+		return "No work as of now";
+	}
+	
+	/**
+	 * Method Name : getWorkDeadline
+	 * @return fixed string "Nil".
+	 */
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+	
+	/**
+	 * Method Name : isTodayAHoliday
+	 * @return fixed string "Today is not a holiday"
+	 */
+	public String isTodayAHoliday() {
+		return "Today is not a holiday";
+	}
+}
